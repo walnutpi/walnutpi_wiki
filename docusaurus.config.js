4f3b43ec0dbb2baf4dab722bead7851deb47b68d
CaptainJackey <237827161@qq.com>
2024-03-25 12:04:30 +0800

    add ZeroW peripherals

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: '核桃派1B教程',
+            label: '核桃派1（1B / ZeroW）教程',
           },
           {
             to: 'picow/directory',
